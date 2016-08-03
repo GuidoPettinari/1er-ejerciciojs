3db93605034c7d9f243a151e6f986d71e8ca5aae
GuidoPettinari <guidopettinari@hotmail.com>
2016-08-03 20:49:56 -0300

    Primer Ejercicio

--- 1er-ejercicio/scripts/1er-ejerciciosv2.js
+++ 1er-ejercicio/scripts/1er-ejerciciosv2.js
@@ -4,14 +4,14 @@
 //El resultado de incrementar en 1 el primer número y el segundo sin modificar.
 
 var prompt1 = parseInt(prompt("Elige un numero del 1 al 10"));
-var prompt2 = parseInt(prompt("Elige un segundo numero del 1 al 10"));
+var prompt2 = parseInt(prompt("Elige un segundo numero del 1 al 10"),22);
 var prompt1sinincremental = prompt1
-var resultado1 = alert(++prompt1);
+var resultado1 = console.log(++prompt1); 
 var resultado2 = alert(prompt2);
 
 
 //El valor de módulo del primer número dividido el segundo.
-var resultado3 = alert (prompt1sinincremental/prompt2);
+var resultado3 = alert (prompt1sinincremental%prompt2);
 //El resultado de restar el primer número y el segundo.
 var resultado4 = alert (prompt1sinincremental - prompt2);
 //El resultado de sumar el primer número con el segundo y ese resultado al primer número.
